04eb8462c96b92834e0b9566cf8162079c7bc003
LeonardoDelBene <leonardo.delbene@stud.unifi.it>
2025-12-17 13:32:15 +0100

    commit project

--- src/main_tracking.py
+++ src/main_tracking.py
@@ -10,19 +10,19 @@ if __name__ == "__main__":
         similarity_threshold="max",  # oppure "max"
         conf_threshold=0.5,
         similarity_mode="net",
-        checkpoint="checkpoints/similarity_net_checkpoint.pth"
+        checkpoint="../checkpoints/similarity_net_checkpoint.pth"
     )
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    tracking = Tracking(detector, max_age=20, n_init=7)
+    tracking = Tracking(detector, max_age=30, n_init=3)
 
     video_path = "../input/video/sportsMot-1"
-    text_input = "a person with blu shirt"
+    text_input = "a person with yellow hoodie"
 
     gt = "../input/video/gt/sportMots-1-gt.txt"
     pred = "../output/video/prediction/sportsMot-1.txt"
 
-    tracking.track_video(video_path, text_input, "../output/video/results.mp4", pred)
-    #tracking.track_realtime(text_input)
+    #tracking.track_video(video_path, text_input, "../output/video/results.mp4", pred)
+    tracking.track_realtime(text_input)
 
     compute_hota(gt, pred)
     res = compute_idf1(gt, pred)
